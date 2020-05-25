7b9280b296b0cf69195b21c98636173c53ce60ef
Rob-Willson <ribrob@gmail.com>
2020-05-25 14:03:53 +0100

    Player prefab now defined, with some basic testing of authority checking

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    [Client]
+    private void Update()
+    {
+        if(!hasAuthority)
+        {
+            return;
+        }
+
+        if(Input.anyKeyDown)
+        {
+            CmdDealTile(1);
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    [Command]
+    private void CmdDealTile(int count)
+    {
+
+
+
+    }
+
+
+
+
+}
