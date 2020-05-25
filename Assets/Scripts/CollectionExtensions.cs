7bd78be596a31ce51d82661c1e3928ec3b04e495
Rob-Willson <ribrob@gmail.com>
2020-05-25 14:08:32 +0100

    Added a extension to shuffle generic collections
    Just a Fisher-Yates shuffle. It relies on Random.Range, so not great, but certainly good enough for our needs (shuffling tiles, etc.)

---
+++ Assets/Scripts/CollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class CollectionExtensions
+{
+    /// <summary>
+    /// Randomly shuffle a collection.
+    /// Based on a Fisher-Yates shuffle, relying on Random.Range().
+    /// So it's fast but not actually particularly random (but good enough for most uses).
+    /// </summary>
+    public static void Shuffle<T> (this IList<T> list)
+    {
+        int n = list.Count;
+        while(n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+}
