47f248c9d87afa0e2925bba11ce4af3675aaba66
Rob-Willson <ribrob@gmail.com>
2020-05-25 14:04:40 +0100

    LetterTile class added with some basic logic for value and support

---
+++ Assets/Scripts/LetterTileData.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterTileData
+{
+    [SerializeField] private readonly char letter;
+    public char Letter
+    {
+        get
+        {
+            return letter;
+        }
+    }
+    [SerializeField] private readonly int value;
+    public int Value
+    {
+        get
+        {
+            return value;
+        }
+    }
+
+    public LetterTileData(char letter)
+    {
+        this.letter = letter;
+        value = new ConvertLetterToValue().GetValue(Letter);
+    }
+
+
+    private sealed class ConvertLetterToValue
+    {
+        Dictionary<char, int> letterValuePairs = new Dictionary<char, int>();
+
+        public ConvertLetterToValue()
+        {
+            GenerateLetterValuePairs();
+        }
+
+        public int GetValue(char letter)
+        {
+            letterValuePairs.TryGetValue(letter, out int value);
+            return value;
+        }
+
+        private void GenerateLetterValuePairs()
+        {
+            letterValuePairs.Add('A', 1);
+            letterValuePairs.Add('B', 3);
+            letterValuePairs.Add('C', 3);
+            letterValuePairs.Add('D', 2);
+            letterValuePairs.Add('E', 1);
+            letterValuePairs.Add('F', 4);
+            letterValuePairs.Add('G', 2);
+            letterValuePairs.Add('H', 4);
+            letterValuePairs.Add('I', 1);
+            letterValuePairs.Add('J', 7);
+            letterValuePairs.Add('K', 5);
+            letterValuePairs.Add('L', 1);
+            letterValuePairs.Add('M', 3);
+            letterValuePairs.Add('N', 1);
+            letterValuePairs.Add('O', 1);
+            letterValuePairs.Add('P', 3);
+            letterValuePairs.Add('Q', 9);
+            letterValuePairs.Add('R', 1);
+            letterValuePairs.Add('S', 1);
+            letterValuePairs.Add('T', 1);
+            letterValuePairs.Add('U', 1);
+            letterValuePairs.Add('V', 4);
+            letterValuePairs.Add('W', 4);
+            letterValuePairs.Add('X', 7);
+            letterValuePairs.Add('Y', 4);
+            letterValuePairs.Add('Z', 9);
+        }
+
+    }
+
+}
