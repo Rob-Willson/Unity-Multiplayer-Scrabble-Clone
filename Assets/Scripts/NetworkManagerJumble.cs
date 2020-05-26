3f3b24f7ccbd80b5c3027da3dc4b5dbe0addacaf
Rob-Willson <ribrob@gmail.com>
2020-05-26 19:03:11 +0100

    Started working on subclass of NetworkManager. I'm not sure this is actually the best way to handle things. Need to better understand how Mirror is handling connections/disconnections

---
+++ Assets/Scripts/NetworkManagerJumble.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Mirror;
+
+public class NetworkManagerJumble : NetworkManager
+{
+    public static NetworkManagerJumble instance = null;
+
+    public static Action ClientJoinedServer;
+
+    public override void Awake()
+    {
+        base.Awake();
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    [SerializeField] private int minimumPlayerCount = 2;
+
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("OnClientConnect");
+        ClientJoinedServer?.Invoke();
+    }
+
+
+}
