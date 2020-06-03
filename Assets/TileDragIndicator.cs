dec37207780a275de18592d775adaa8b707d3918
Rob-Willson <ribrob@gmail.com>
2020-06-03 11:44:36 +0100

    A tile 'ghost' now indicates when a drag is ongoing on the local client

---
+++ Assets/TileDragIndicator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class TileDragIndicator : MonoBehaviour
+{
+    private Transform tr;
+    private Transform Tr
+    {
+        get
+        {
+            if(tr == null)
+            {
+                tr = transform;
+            }
+            return tr;
+        }
+    }
+
+    [SerializeField] private MeshRenderer meshRenderer = null;
+
+    private void Awake()
+    {
+        Hide();
+    }
+
+    private void OnEnable()
+    {
+        PlayerInput.TileDragOngoing += MoveToPosition;
+        PlayerInput.TileDragEnd += Hide;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInput.TileDragOngoing -= MoveToPosition;
+        PlayerInput.TileDragEnd -= Hide;
+    }
+
+    private void MoveToPosition (Vector3 newPosition)
+    {
+        // Offset above other tile slightly so that it stands out above other tiles
+        newPosition += new Vector3(0, 0.1f, 0);
+
+        Tr.position = newPosition;
+        meshRenderer.gameObject.SetActive(true);
+    }
+
+    private void Hide ()
+    {
+        meshRenderer.gameObject.SetActive(false);
+    }
+
+
+
+}
