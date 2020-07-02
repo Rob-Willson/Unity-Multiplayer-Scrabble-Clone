ab401f23f74731c6fa08a33f8b4a3b75af95026e
Rob-Willson <ribrob@gmail.com>
2020-07-02 23:19:16 +0100

    World camera is now automatically selected on start

---
+++ Assets/UI_CanvasSetWorldCameraOnStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class UI_CanvasSetWorldCameraOnStart : MonoBehaviour
+{
+    private void Start()
+    {
+        Canvas canvas = GetComponent<Canvas>();
+        if(canvas == null)
+        {
+            Debug.LogError("FAIL: Canvas not found on object");
+            return;
+        }
+
+        canvas.worldCamera = CameraCalculations.MainCamera;
+    }
+}
