7087a9d39056a4e8860ae5826656b831b75395f2
Rob-Willson <ribrob@gmail.com>
2020-07-02 23:16:18 +0100

    Disgusting mobile-friendly fix for certain non-mobile UI elements

---
+++ Assets/UI_DisableGameObjectOnMobile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_DisableGameObjectOnMobile : MonoBehaviour
+{
+
+    private void Start()
+    {
+        if(Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+}
