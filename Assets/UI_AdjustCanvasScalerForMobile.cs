26a064199f28230c9d4a3f45688e9b2dc723f629
Rob-Willson <ribrob@gmail.com>
2020-07-02 23:17:01 +0100

    Disgusting mobile-friendly fix for certain UI elements

---
+++ Assets/UI_AdjustCanvasScalerForMobile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class UI_AdjustCanvasScalerForMobile : MonoBehaviour
+{
+    #if UNITY_ANDROID
+
+    private void Start()
+    {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            ScaleForMobile();
+        }
+    }
+
+    private void ScaleForMobile ()
+    {
+        CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
+        canvasScaler.scaleFactor = 1.5f;
+    }
+
+    #endif
+
+}
